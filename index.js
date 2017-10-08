4bf290f65c138a20325b418948cd669be59e0d17
rosie w <rosie.wilt@gmail.com>
2017-10-08 20:27:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ return 'Happy holidays!'
 }
 
 function happyHolidaysTo(name) {
-  console.log('Happy holidays, ${name}!')
+  console.log("Happy holidays, ${name}!")
 }
 
 function happyHolidayTo(holiday, name) {
